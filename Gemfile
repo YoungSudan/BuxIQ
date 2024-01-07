3d2910f4b8ea89248b85dc0b9574ac233aff4fca
YoungSudan <lochalamoicolin83@gmail.com>
2024-01-07 11:38:25 -0500

    create controllers for budget,dashboard,settings and expenses

--- Gemfile
+++ Gemfile
@@ -68,3 +68,4 @@ end
 
 gem "devise"
 gem 'annotate'
+gem 'plaid'
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,8 @@
 #  remember_created_at    :datetime
 #  created_at             :datetime         not null
 #  updated_at             :datetime         not null
+#  first_name             :string
+#  last_name              :string
 #
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
